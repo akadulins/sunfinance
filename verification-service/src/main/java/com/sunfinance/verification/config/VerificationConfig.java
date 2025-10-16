50c8f86d516bc93e8d9b93e8a861022f1062e2d9
Aleksejs Kadulins <aleksejs.kadulins@gmail.com>
2025-10-16 17:12:12 +0200

    Added rest template bean to config class

--- verification-service/src/main/java/com/sunfinance/verification/config/VerificationConfig.java
+++ verification-service/src/main/java/com/sunfinance/verification/config/VerificationConfig.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +24,11 @@ public class VerificationConfig {
 
     public Duration getValidityPeriod() { return validityPeriod; }
     public void setValidityPeriod(Duration validityPeriod) { this.validityPeriod = validityPeriod; }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
     
     @Bean
     public ObjectMapper objectMapper() {
